a70f1a708d34f03ad89aace28ddc1f075635b3bc
Nezabravka <nezabravka@test.com>
2025-06-27 22:37:19 +0300

    Commit 3 chore

--- script.js
+++ script.js
@@ -18,3 +18,4 @@ document.getElementById('btn-subtract').addEventListener('click', () => {
   document.getElementById('result').textContent = subtract(a, b);
 });
 //commit 1 chore
+//commit 3 chore
